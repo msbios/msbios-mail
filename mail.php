09b21df766cf8adceda8476b8bf2cf565e67dbb7
Judzhin Miles <info@msbios.com>
2018-10-31 16:38:26 +0200

    INtermedaite commit

---
+++ mail.php
@@ -0,0 +1,75 @@
+<?php
+
+use Zend\Mail\Message;
+use Zend\Mail\Transport\Smtp as SmtpTransport;
+use Zend\Mime\Message as MimeMessage;
+use Zend\Mime\Mime;
+use Zend\Mime\Part as MimePart;
+use Zend\Mail\Transport\SmtpOptions;
+
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
+}
+// Composer autoloading
+include __DIR__ . '/vendor/autoload.php';
+
+if (!class_exists(Message::class)) {
+    throw new RuntimeException(
+        "Unable to load application.\n"
+        . "- Type `composer install` if you are developing locally.\n"
+        . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
+        . "- Type `docker-compose run zf composer install` if you are using Docker.\n"
+    );
+}
+
+/** @var string $htmlMarkup */
+$htmlMarkup = file_get_contents(__DIR__ . '/template.phtml');
+
+/** @var \Zend\Mime\Part $html */
+// $html = new MimePart($htmlMarkup);
+$html = new MimePart($htmlMarkup);
+$html->type = Mime::TYPE_HTML;
+$html->charset = 'utf-8';
+$html->encoding = Mime::ENCODING_QUOTEDPRINTABLE;
+$html->disposition = Mime::DISPOSITION_INLINE;
+
+/** @var \Zend\Mime\Message $body */
+$body = new MimeMessage;
+$body->addPart($html);
+
+/** @var Message $message */
+$message = new Message;
+$message
+    ->setBody($body)
+    ->setFrom('info@gmail.com', 'Test User')
+    ->setTo('judzhin@gns-it.com', "Judzhin Miles")
+    ->setSubject('inf');
+
+/** @var \Zend\Mail\Transport\TransportInterface $transport */
+$transport = new SmtpTransport();
+
+/** @var \Zend\Stdlib\ParameterObjectInterface $options */
+$options = new SmtpOptions([
+    'name' => 'gmail.com',
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+    'connection_class' => 'login',
+    'connection_config' => [
+        'username' => '',
+        'password' => '',
+        'ssl' => 'tls',
+    ]
+]);
+
+$transport->setOptions($options);
+$transport->send($message);
